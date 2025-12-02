0f9a5ee658131c7e09870ce3c33817c415bc44d9
abb00717 <abb00717@gmail.com>
2025-12-02 19:59:15 +0800

    style: Remove legacy input_mode

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-#include <cstddef>
-#include <cstdint>
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -7,13 +5,7 @@
 
 #define TAPE_SIZE 1024
 
-enum input_mode {
-    input_mode_stdin,
-    input_mode_file,
-};
-
 int main(int argc, char *argv[]) {
-    input_mode input_mode;
     // Read the input from file or STDIN
     std::string code;
     if (argc == 1) {
